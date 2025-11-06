23eb0cc1d762abbe338746e8a7b4508efa47aeb0
miaravnanger <mia_maroen@hotmail.com>
2025-11-06 13:53:06 +0100

    layout

--- src/Components/Dish.jsx
+++ src/Components/Dish.jsx
@@ -1,13 +1,17 @@
-
 import styles from "./Dish.module.css";
 
-export default function Dish({tittel, pris, ingredienser}) {
-
+export default function Dish({ tittel, pris, ingredienser }) {
   return (
-    <div className={styles.card}>
-      <h3>{tittel}</h3>
-      <p>{ingredienser}</p>
-      <p>{pris}</p>
-    </div>
+    <>
+      <div className={styles.card}>
+        <div className={styles.header}>
+          <h3>{tittel}</h3>
+          <span className={styles.price}>
+            <p>{pris}</p>
+          </span>
+        </div>
+        <p>{ingredienser}</p>
+      </div>
+    </>
   );
 }
