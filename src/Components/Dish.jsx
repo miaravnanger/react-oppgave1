3092260145b594fdf578a6b5705afad920026284
miaravnanger <mia_maroen@hotmail.com>
2025-11-07 09:51:53 +0100

    grid

--- src/Components/Dish.jsx
+++ src/Components/Dish.jsx
@@ -4,13 +4,9 @@ export default function Dish({ tittel, pris, ingredienser }) {
   return (
     <>
       <div className={styles.card}>
-        <div className={styles.header}>
           <h3>{tittel}</h3>
-          <span className={styles.price}>
-            <p>{pris}</p>
-          </span>
-        </div>
-        <p>{ingredienser}</p>
+            <h4 className={styles.price}>{pris}</h4>
+        <h4 className={styles.ing}>{ingredienser}</h4>
       </div>
     </>
   );
